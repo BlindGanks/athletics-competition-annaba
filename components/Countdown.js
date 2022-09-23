fb101340922cb33c8089a4a6b4b60a014d73b330
blindganks <abdelhaktebak@gmail.com>
2022-09-23 15:19:39 +0100

    fixed react/display-name error

--- components/Countdown.js
+++ components/Countdown.js
@@ -75,7 +75,7 @@ const Countdown = () => {
     </div>
   );
 };
-const Timer = memo(({ time, text }) => (
+const Timer = ({ time, text }) => (
   <div className="w-[75px] h-[68px] md:w-[115px] md:h-[108px] lg:h-[163px] lg:w-[170px] xl:h-[210px] xl:w-[233px] bg-redPrimary text-center flex flex-col justify-center text-white">
     <span className="block font-futura text-4xl md:text-[3.25rem] lg:text-[6.6rem] md:leading-[3rem] lg:leading-[6.25rem]">
       {time}
@@ -84,5 +84,5 @@ const Timer = memo(({ time, text }) => (
       {text}
     </span>
   </div>
-));
+);
 export default Countdown;
